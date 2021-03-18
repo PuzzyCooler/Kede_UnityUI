69c6f96f881c7abc2940d992644581ab6620cbfd
rejnsKED <asknelogical@gmail.com>
2021-03-18 17:28:55 +0200

    Pievienoti 2 teli

---
+++ Assets/Skripts/teli.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class teli : MonoBehaviour
+{
+    public GameObject mainigaisAttels;
+    public Sprite[] attelumasivs;
+    public void izkritosais(int skaitlis){
+        if(skaitlis == 0){
+            mainigaisAttels.GetComponent<Image>().sprite = attelumasivs[0];
+        }else if(skaitlis == 1) {
+            mainigaisAttels.GetComponent<Image>().sprite = attelumasivs[1];
+        }
+    }
+}
