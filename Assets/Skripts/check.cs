7c5eac67fa787b947daaa2cf3a5d53bc078e826f
rejnsKED <asknelogical@gmail.com>
2021-03-18 19:06:19 +0200

    Pievienoju drebju kategoriju

---
+++ Assets/Skripts/check.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class check : MonoBehaviour {
+    public GameObject cepure;
+    public GameObject dzemperis;
+    public GameObject bikses;
+    public GameObject kurpe;
+
+
+    public void hatatelosana(bool vertiba){
+        cepure.SetActive(vertiba);
+    }
+    public void dzemperisatelosana(bool vertiba){
+        dzemperis.SetActive(vertiba);
+    }
+    public void biksesatelosana(bool vertiba){
+        bikses.SetActive(vertiba);
+    }
+    public void kurpeatelosana(bool vertiba){
+        kurpe.SetActive(vertiba);
+    }
+}
